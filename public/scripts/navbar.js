27428cb130f1edc0d324f64f64379c6ea1b09973
Furkan Taş <73059531+furrkanntas@users.noreply.github.com>
2024-08-03 16:20:28 +0300

    v1.3 | Updated the interface view on the project page.

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,10 +1,10 @@
 document.getElementById('toggleMenu').addEventListener('click', function () {
     var menu = document.getElementById('ftmenu');
-    this.classList.toggle('rotate'); 
+    this.classList.toggle('rotate');
 
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
-        
+
         setTimeout(function () {
             menu.style.maxHeight = menu.scrollHeight + 'px';
             menu.style.maxWidth = '100%';
@@ -16,4 +16,17 @@ document.getElementById('toggleMenu').addEventListener('click', function () {
             menu.classList.add('hidden');
         }, { once: true });
     }
+});
+
+document.querySelectorAll('.hoverEffect').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.querySelectorAll('.topicFt, .textFt, .techFt, .buttonFts').forEach(element => {
+            element.classList.remove('hidden');
+        });
+    });
+    item.addEventListener('mouseout', () => {
+        item.querySelectorAll('.topicFt, .textFt, .techFt, .buttonFts').forEach(element => {
+            element.classList.add('hidden');
+        });
+    });
 });
